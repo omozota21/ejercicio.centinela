2b924c850d8263ef47e3cd8c3e38aedf2214a20a
omozota21 <omozota21@iesrioarba.es>
2023-10-25 12:10:49 +0200

    ç

--- Bucles.c
+++ Bucles.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
-
+#include END -1
+#include sizeof(vector) / sizeof(vector[0])
 
 int main() {
-    int vector [10] = {11,22,33,44,55,66,77,88,99};
-    int vector [10] = {11,22,33,44,END,66,77,88,99};    // 2
+    // int vector [10] = {11,22,33,44,55,66,77,88,99};
+    // int vector [10] = {11,22,33,44,END,66,77,88,99};    // 2
     int vector [10] = {END,22,33,44,55,66,77,88,99};    // 3
-    int vector [10] = {11,22,33,44,55,66,77,88,99,END}; // 4 
-    int vector [10] = {11,END};                         // 5
-```
-    
+    // int vector [10] = {11,22,33,44,55,66,77,88,99,END}; // 4 
+    // int vector [10] = {11,END};                         // 5
+
     
     int longitud = sizeof(vector) / sizeof(vector[0]);
     
-    int datos_validos = 0;
-    
-    for (int i = 0; i < longitud; i++) {
-        if (vector[i] > 0) {
-            printf("Dato válido: %d\n", vector[i]);
-            datos_validos++;    
+   
+    int i = 0;
+    if (TAMV(vector) != 0);
+    {
+        for (i = 0; i < longitud; i++) 
+        {
+        
         }
     }
-    printf("Total de datos válidos: %d\n", datos_validos);
-    return 0;
+    
+    
+    
 }
